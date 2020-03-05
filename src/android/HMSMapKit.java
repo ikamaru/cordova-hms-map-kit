561fbecdb1cc6f457cfcdfea4fbb0f7afe749e3a
ikamaru <ikamaru.ikuane@gmail.com>
2020-03-05 23:09:37 +0100

    add markers

--- src/android/HMSMapKit.java
+++ src/android/HMSMapKit.java
@@ -24,6 +24,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import com.huawei.hms.maps.MapView;
 import com.huawei.hms.maps.CameraUpdate;
 import com.huawei.hms.maps.CameraUpdateFactory;
@@ -36,7 +38,7 @@ import com.huawei.hms.maps.model.CircleOptions;
 import com.huawei.hms.maps.model.LatLng;
 import com.huawei.hms.maps.model.Marker;
 import com.huawei.hms.maps.model.MarkerOptions;
-import com.huawei.hms.maps.util.LogM;
+  import com.huawei.hms.maps.util.LogM;
 
 import androidx.core.app.ActivityCompat;
 
@@ -45,14 +47,12 @@ import androidx.core.app.ActivityCompat;
  * This class echoes a string called from JavaScript.
  */
 public class HMSMapKit extends CordovaPlugin {
-  private static final String[] RUNTIME_PERMISSIONS = {Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    private static final String[] RUNTIME_PERMISSIONS = {Manifest.permission.WRITE_EXTERNAL_STORAGE,
     Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_COARSE_LOCATION,
     Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET};
     private static final int REQUEST_CODE = 100;
     private static final LatLng LAT_LNG = new LatLng(31.2304, 121.4737);
     private static final String TAG = "MapViewDemoActivity";
-  private Marker mMarker;
-  private Circle mCircle;
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
     }
@@ -63,13 +63,8 @@ public class HMSMapKit extends CordovaPlugin {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
             return true;
-        }else if(action.equals("addTwoNumbers")) {
-            Double first = args.getDouble(0);
-            Double second = args.getDouble(1);
-            this.addTwoNumbers(first,second, callbackContext);
-            return true;
-        }else if(action.equals("loadMap")) {
-            this.loadMap();
+        }else if(action.equals("loadMapWithMarkers")) {
+            this.loadMapWithMarkers(args.getJSONObject(0));
             return true;
         }
         return false;
@@ -82,13 +77,7 @@ public class HMSMapKit extends CordovaPlugin {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
-    private void addTwoNumbers(Double first,Double second, CallbackContext callbackContext) {
-        if (first != null && second != null) {
-            callbackContext.success(""+(first+second));
-        } else {
-            callbackContext.error("Expected two non-empty double argument.");
-        }
-    }
+
     /*private void loadMap() {
         cordova.getActivity().runOnUiThread(new Runnable() {
             @Override
@@ -100,17 +89,21 @@ public class HMSMapKit extends CordovaPlugin {
         });
 
     }*/
-    private synchronized void loadMap(){
+    private synchronized void loadMapWithMarkers(JSONObject markers){
+      Log.d(TAG, "onMap not Ready: ");
       final CordovaInterface cordova=this.cordova;
       Runnable runnable=new Runnable() {
         @Override
         public void run() {
           AlertDialog.Builder dlg = new AlertDialog.Builder(cordova.getActivity(), AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
           Application app=cordova.getActivity().getApplication();
+
           String package_name=app.getPackageName();
           Resources resources=app.getResources();
+
           int layout =resources.getIdentifier("activity_new","layout",package_name);
           int map=resources.getIdentifier("mapView","id",package_name);
+
           LayoutInflater inflater=cordova.getActivity().getLayoutInflater();
           View customView=inflater.inflate(layout, null);
           dlg.setView(customView);
@@ -128,29 +121,63 @@ public class HMSMapKit extends CordovaPlugin {
           mMapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(HuaweiMap huaweiMap) {
+
               Log.d(TAG, "onMapReady: ");
+              try {
+                Toast.makeText(cordova.getContext(), "onMapReady:", Toast.LENGTH_SHORT).show();
+                // after call getMapAsync method ,we can get HuaweiMap instance in this call back method
+                HuaweiMap hmap = huaweiMap;
+                hmap.setMyLocationEnabled(true);
 
-              // after call getMapAsync method ,we can get HuaweiMap instance in this call back method
-              HuaweiMap hmap = huaweiMap;
-              hmap.setMyLocationEnabled(true);
+                // move camera by CameraPosition param ,latlag and zoom params can set here
+                CameraPosition build = new CameraPosition.Builder().target(new LatLng(60, 60)).zoom(5).build();
 
-              // move camera by CameraPosition param ,latlag and zoom params can set here
-              CameraPosition build = new CameraPosition.Builder().target(new LatLng(60, 60)).zoom(5).build();
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(build);
+                hmap.animateCamera(cameraUpdate);
+                hmap.setMaxZoomPreference(20);
+                hmap.setMinZoomPreference(2);
+                JSONArray markersArr=markers.getJSONArray("markers");
+                for(int i=0;i<markersArr.length();i++){
+                  Log.d(TAG, "onMapReady: sni "+markersArr.getJSONObject(i).getString("title")+" "+markersArr.getJSONObject(i).getString("lng")+" "+markersArr.getJSONObject(i).getString("lat")+" "+markersArr.getJSONObject(i).getString("snippet"));
+                  Marker tmpMarker = hmap.addMarker(new MarkerOptions()
+                    .position(new LatLng(Double.parseDouble(markersArr.getJSONObject(i).getString("lat")), Double.parseDouble(markersArr.getJSONObject(i).getString("lng"))))
+                    .snippet(markersArr.getJSONObject(i).getString("snippet"))
+                    .title(markersArr.getJSONObject(i).getString("title"))
+                    .icon(BitmapDescriptorFactory.fromResource(resources.getIdentifier("badge_ph","drawable",package_name)))
+                    .clusterable(true));
+                }
+                /*
 
-              CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(build);
-              hmap.animateCamera(cameraUpdate);
-              hmap.setMaxZoomPreference(5);
-              hmap.setMinZoomPreference(2);
-
-              // mark can be add by HuaweiMap
-              mMarker = hmap.addMarker(new MarkerOptions().position(LAT_LNG)
-                .icon(BitmapDescriptorFactory.fromResource(resources.getIdentifier("badge_ph","drawable",package_name)))
-                .clusterable(true));
-
-              mMarker.showInfoWindow();
-
-              // circle can be add by HuaweiMap
-              mCircle = hmap.addCircle(new CircleOptions().center(new LatLng(60, 60)).radius(5000).fillColor(Color.GREEN));
+                for(int i=0;i<markersArr.length();i++){
+                  Marker tmpMarker = hmap.addMarker(new MarkerOptions()
+                    .position(new LatLng(Double.parseDouble(markersArr.getJSONObject(i).getString("lat")), Double.parseDouble(markersArr.getJSONObject(i).getString("lng"))))
+                    .snippet(markersArr.getJSONObject(i).getString("snippet"))
+                    .title(markersArr.getJSONObject(i).getString("title"))
+                    .icon(BitmapDescriptorFactory.fromResource(resources.getIdentifier("badge_ph","drawable",package_name)))
+                    .clusterable(true));
+                  //tmpMarker.showInfoWindow();
+                }*/
+                hmap.setOnMarkerClickListener(new HuaweiMap.OnMarkerClickListener() {
+                  @Override
+                  public boolean onMarkerClick(Marker marker) {
+                    //send an event to typescript
+                    Toast.makeText(cordova.getContext(), "title:" + marker.getTitle(), Toast.LENGTH_SHORT).show();
+                    return false;
+                  }
+                });
+                hmap.setOnInfoWindowClickListener(new HuaweiMap.OnInfoWindowClickListener() {
+                  @Override
+                  public void onInfoWindowClick(Marker marker) {
+                    Toast.makeText(cordova.getContext(), "snippet:" + marker.getSnippet(), Toast.LENGTH_SHORT).show();
+                  }
+                });
+                Log.d(TAG, "onMapShow: ");
+                //callbackContext.success("nice");
+              }catch(Exception e){
+                Log.d(TAG, "onMapErrJson: "+e.getMessage());
+                Toast.makeText(cordova.getContext(), "onMapErrJson: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                //callbackContext.error("onMapErrJson: "+e.getMessage());
+              }
             }
           });
           dlg.create();
--- www/HMSMapKit.js
+++ www/HMSMapKit.js
@@ -3,9 +3,17 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'HMSMapKit', 'coolMethod', [arg0]);
 };
-exports.addTwoNumbers = function (arg0,arg1, success, error) {
-    exec(success, error, 'HMSMapKit', 'addTwoNumbers', [arg0,arg1]);
-};
-exports.loadMap = function () {
-    exec(function(res){}, function(err){}, 'HMSMapKit', 'loadMap', []);
+/*
+{"markers":[
+	{
+		"lat":"12.231233",
+		"lng":"12.223123",
+		"snippet":"snippet here",
+		"title":"title here"
+	}
+]}
+
+*/
+exports.loadMapWithMarkers = function (markersJson,success,error) {
+    exec(function(res){}, function(err){}, 'HMSMapKit', 'loadMapWithMarkers', [markersJson]);
 };
